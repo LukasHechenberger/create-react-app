8a65a250f1aa7374dcd77c97622d15c913065a6f
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2020-08-03 11:29:35 +0200

    Adaptions for create-atvise-app (#1)
    * Use index.htm as entry file in production builds
    
    * Rename react-scripts to @atvise/react-scripts
    
    * Inject webmi.js in templates
    
    * Rename templates
    
    * Add atvise packages to templates
    
    * Document deployment script
    
    * Add atvise variable label to templates

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useSubscription } from '@atvise/webmi-react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const { data } = useSubscription('AGENT.OBJECTS.test');
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +21,10 @@ function App() {
         >
           Learn React
         </a>
+        <hr />
+        <p>
+          <code>AGENT.OBJECTS.test</code> is currently: <strong>{data?.value.toFixed(2) ?? ''}</strong>
+        </p>
       </header>
     </div>
   );
